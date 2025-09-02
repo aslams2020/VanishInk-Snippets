6644acefe8aec99f76dc88b69f3b44e58c314ce0
Sayyad Aslam <sayyadaslam2020@gmail.com>
2025-09-02 19:12:51 +0530

    Updated VanishRepo

--- src/main/java/in/sb/vink/repository/VanishRepository.java
+++ src/main/java/in/sb/vink/repository/VanishRepository.java
@@ -13,10 +13,10 @@ import in.sb.vink.model.Vanish;
 @Repository
 public interface VanishRepository extends JpaRepository<Vanish, Long> {
 
-    // Use a native SQL query for absolute precision
+   
     @Query(value = "SELECT * FROM vanish v WHERE v.vanish_id = :vanishId", nativeQuery = true)
     Optional<Vanish> findByVanishId(@Param("vanishId") String vanishId);
 
-    // This method will be used by the scheduler to delete expired Vanishes
+    // used by the scheduler to delete expired Vanishes..
     void deleteByExpiresAtBefore(LocalDateTime currentTime);
 }
